b3c37892870187d890ac69f09501e75f0531e615
Sudo_free <sudofree@163.com>
2015-05-24 01:54:54 +0800

    bug fixed, data bind partly, not success

--- www/javascripts/styleFunc.js
+++ www/javascripts/styleFunc.js
@@ -1,9 +1,7 @@
 function alert_flash(message){
 	var ndiv = "<div id='alert_flash'>" + message + "</div>";
-	$("body").append(ndiv);
-	$("#alert_flash").css({
-		"top" : "90%",
-		"left" : "35%"
-	}).show(2000);
-	setTimeout("$('#alert_flash').fadeOut(3000).remove()", 3000);
+	if (!$('#alert_flash').length) {
+		$("body").append(ndiv);
+		$("#alert_flash").show(2000);
+		setTimeout("$('#alert_flash').fadeOut(1000, function(){this.remove()})", 3000);}
 }
