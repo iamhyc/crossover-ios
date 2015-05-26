21aecf84539ee63185108e967d3a98ed15d0038c
Sudo_free <sudofree@163.com>
2015-05-26 12:08:31 +0800

    style fixed, Fight_Module to be added

--- www/javascripts/styleFunc.js
+++ www/javascripts/styleFunc.js
@@ -1,7 +1,8 @@
 function alert_flash(message){
-	var ndiv = "<div id='alert_flash'>" + message + "</div>";
+	var ndiv = "<div id='alert_flash' style='color:#000000'>" + message + "</div>";
 	if (!$('#alert_flash').length) {
 		$("body").append(ndiv);
+		$("#alert_flash").css("width", message.length*20+"px");
 		$("#alert_flash").show(2000);
 		setTimeout("$('#alert_flash').fadeOut(1000, function(){this.remove()})", 3000);}
 }
