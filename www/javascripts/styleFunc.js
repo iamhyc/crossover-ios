9024762ce35ecb8849c8413a61045881bb144761
Sudo_free <sudofree@163.com>
2015-05-30 01:32:49 +0800

    I FORMAT THE SCORELIST AND ADD COMMENT ON IT.REALLY HEAVY WORK

--- www/javascripts/styleFunc.js
+++ www/javascripts/styleFunc.js
@@ -1,8 +1,10 @@
-function alert_flash(message){
+function alert_flash(message, callback){
 	var ndiv = "<div id='alert_flash' style='color:#000000'>" + message + "</div>";
 	if (!$('#alert_flash').length) {
 		$("body").append(ndiv);
 		$("#alert_flash").css("width", message.length*25+"px");
 		$("#alert_flash").show(2000);
-		setTimeout("$('#alert_flash').fadeOut(1000, function(){this.remove()})", 3000);}
+		setTimeout("$('#alert_flash').fadeOut(1000, function(){this.remove()})", 3000);
+		}
+	if (callback) return callback();
 }
