cc29b5388643cb2ef65100d6ba5f2bfa18ece90c
Sudo_free <sudofree@163.com>
2015-06-12 18:52:05 +0800

    Basic function done.Delete Method to be added

--- www/javascripts/styleFunc.js
+++ www/javascripts/styleFunc.js
@@ -33,19 +33,48 @@ function alert_flash(message, callback){
 }
 
 function alert_modal (message, callback) {
-	var alert_modal = '<div id="alert_modal"><div style="height:30%"><p style="color:#3CB3B3;font-size:1.6em;padding-left:4%;padding-top:5%">确认分数</p></div><div style="height:1%;background:#3CB3B3"></div><div style="height:47%"><p style="padding-left:5%;padding-top:7%">'+message+'</p><input type ="password" placeholder="输入密码"  autofocus class="modal_input"></input></div><div style = "height:22%"><button type="button" class="modal_button" onclick="getPwd()">确定</button></div></div>';
-	if (!$('#alert_modal').length) {
-		$("body").append(modal0);
+	
+	Eliminate = function () {
+			$("#alert_modal").remove();
 		}
+
+	var alert_modal = '<div id="alert_modal" class="def_modal"><div style="height:30%"><p style="color:#3CB3B3;font-size:1.6em;padding-left:4%;padding-top:5%">提示</p></div><div style="height:1%;background:#3CB3B3"></div><div style="height:37%"><p style="padding-left:5%;padding-top:7%">'+message+'</p></div><div style="height:22%"><button type="button" class="modal_button" onclick="Eliminate()">确定</button></div></div>';
+	
+	if (!$('#alert_modal').length) {
+		$("body").append(alert_modal);
+	}
+	if (callback) return callback();
 }
 
-function getPwd (){
-	$('.prompt_modal').remove();
+function confirm_modal (message, callback) {
+	
+	setVal = function(val){
+		this.Eliminate();
+		if (val == 1&&callback) callback();
+	}
+
+	Eliminate = function () {
+		$("#confirm_modal").remove();
+	}
+
+	var confirm_modal = '<div id="confirm_modal" class="def_modal"><div style="height:30%"><p style="color:#3CB3B3;font-size:1.6em;padding-left:4%;padding-top:5%">提示</p></div><div style="height:1%;background:#3CB3B3"></div><div style="height:37%"><p style="padding-left:5%;padding-top:7%">'+message+'</p></div><div class="row" style="margin:0;height:22%"><button type="button" class="modal_button" style="width:50%" onclick="setVal(0)">取消</button><button type="button" class="modal_button" style="width:50%" onclick="setVal(1)">确定</button></div></div>';
+	if (!$('#confirm_modal').length) {
+		$("body").append(confirm_modal);
+	}
+	if (callback) return callback();
 }
-function prompt_modal (message) {
+
+
+function prompt_modal (message, callback) {
 	// Type 0 : Confirming
-	var modal0 = '<div id="prompt_modal"><div style="height:30%"><p style="color:#3CB3B3;font-size:1.6em;padding-left:4%;padding-top:5%">确认分数</p></div><div style="height:1%;background:#3CB3B3"></div><div style="height:47%"><p style="padding-left:5%;padding-top:7%">'+message+'</p><input type ="password" placeholder="输入密码"  autofocus class="modal_input"></input></div><div style = "height:22%"><button type="button" class="modal_button" onclick="getPwd()">确定</button></div></div>';
+	getPwd = function(){
+		var pwd = $("#prompt_modal .modal_input").val();
+		callback(pwd);
+		$("#prompt_modal").remove();
+	}
+	
+	var modal0 = '<div id="prompt_modal" class="def_modal"><div style="height:30%"><p style="color:#3CB3B3;font-size:1.6em;padding-left:4%;padding-top:5%">确认分数</p></div><div style="height:1%;background:#3CB3B3"></div><div style="height:47%"><p style="padding-left:5%;padding-top:7%">'+message+'</p><input type ="password" placeholder="输入密码"  autofocus class="modal_input"></input></div><div style="height:22%"><button type="button" class="modal_button" onclick="getPwd()">确定</button></div></div>';
 	if (!$('.prompt_modal').length) {
 		$("body").append(modal0);
-		}
+	}
 }
