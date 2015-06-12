4638491a91c7522940f9758afac0adef6d698949
Sudo_free <sudofree@163.com>
2015-06-12 21:19:16 +0800

    delete function added

--- www/javascripts/styleFunc.js
+++ www/javascripts/styleFunc.js
@@ -7,11 +7,14 @@ function stopBubble (e) {
 }
 
 //长按事件
-function longFocus (eid, callback) {
+function longFocus (qObj, callback) {
 	var timer = null;
-	$("#"+eid).on({
+	call = callback;
+
+	qObj.on({
 		mousedown:function(){
-			timer = setTimeout(callback, 2000 );
+			qid = $(this).attr("id");
+			timer = setTimeout('call(qid)', 500)
 		},
 		mouseup:function(){
 			clearTimeout( timer );
@@ -36,11 +39,13 @@ function alert_modal (message, callback) {
 	
 	Eliminate = function () {
 			$("#alert_modal").remove();
+			$("#black-bg").css("display", "none");
 		}
 
 	var alert_modal = '<div id="alert_modal" class="def_modal"><div style="height:30%"><p style="color:#3CB3B3;font-size:1.6em;padding-left:4%;padding-top:5%">提示</p></div><div style="height:1%;background:#3CB3B3"></div><div style="height:37%"><p style="padding-left:5%;padding-top:7%">'+message+'</p></div><div style="height:22%"><button type="button" class="modal_button" onclick="Eliminate()">确定</button></div></div>';
 	
 	if (!$('#alert_modal').length) {
+		$("#black-bg").css("display", "block");
 		$("body").append(alert_modal);
 	}
 	if (callback) return callback();
@@ -55,13 +60,15 @@ function confirm_modal (message, callback) {
 
 	Eliminate = function () {
 		$("#confirm_modal").remove();
+		$("#black-bg").css("display", "none");
 	}
 
 	var confirm_modal = '<div id="confirm_modal" class="def_modal"><div style="height:30%"><p style="color:#3CB3B3;font-size:1.6em;padding-left:4%;padding-top:5%">提示</p></div><div style="height:1%;background:#3CB3B3"></div><div style="height:37%"><p style="padding-left:5%;padding-top:7%">'+message+'</p></div><div class="row" style="margin:0;height:22%"><button type="button" class="modal_button" style="width:50%" onclick="setVal(0)">取消</button><button type="button" class="modal_button" style="width:50%" onclick="setVal(1)">确定</button></div></div>';
+
 	if (!$('#confirm_modal').length) {
+		$("#black-bg").css("display", "block");
 		$("body").append(confirm_modal);
 	}
-	if (callback) return callback();
 }
 
 
@@ -71,10 +78,12 @@ function prompt_modal (message, callback) {
 		var pwd = $("#prompt_modal .modal_input").val();
 		callback(pwd);
 		$("#prompt_modal").remove();
+		$("#black-bg").css("display", "none");
 	}
 	
 	var modal0 = '<div id="prompt_modal" class="def_modal"><div style="height:30%"><p style="color:#3CB3B3;font-size:1.6em;padding-left:4%;padding-top:5%">确认分数</p></div><div style="height:1%;background:#3CB3B3"></div><div style="height:47%"><p style="padding-left:5%;padding-top:7%">'+message+'</p><input type ="password" placeholder="输入密码"  autofocus class="modal_input"></input></div><div style="height:22%"><button type="button" class="modal_button" onclick="getPwd()">确定</button></div></div>';
 	if (!$('.prompt_modal').length) {
+		$("#black-bg").css("display", "block");
 		$("body").append(modal0);
 	}
 }
