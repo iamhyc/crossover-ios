a5555eeba5593f14a1ba31092824cf3e6bfadc49
Sudo_free <sudofree@163.com>
2015-06-21 01:24:23 +0800

    By HKJ styleModify v5

--- www/javascripts/styleFunc.js
+++ www/javascripts/styleFunc.js
@@ -28,7 +28,7 @@ function alert_flash(message, callback){
 	var ndiv = "<div id='alert_flash'>" + message + "</div>";
 	if (!$('#alert_flash').length) {
 		$("body").append(ndiv);
-		$("#alert_flash").css("width", message.length*25+"px");
+		$("#alert_flash").css("width", message.length*20+"px");
 		$("#alert_flash").show(1500);
 		setTimeout("$('#alert_flash').fadeOut(500, function(){this.remove()})", 2500);
 		}
